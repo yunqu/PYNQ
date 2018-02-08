80bde80075a5d13fe0a58f3d8c92e36a78dc7f34
Rock Qu <yunq@xilinx.com>
2018-02-08 12:25:10 -0800

    add headers for mailbox src

--- boards/sw_repo/mailbox_bram/src/mailbox_io.c
+++ boards/sw_repo/mailbox_bram/src/mailbox_io.c
@@ -1,10 +1,52 @@
-/*
- * mailbox_io.c
+/******************************************************************************
+ *  Copyright (c) 2018, Xilinx, Inc.
+ *  All rights reserved.
  *
- *  Created on: 16 Feb 2017
- *      Author: ogden
- */
-
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  1.  Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *
+ *  2.  Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *
+ *  3.  Neither the name of the copyright holder nor the names of its
+ *      contributors may be used to endorse or promote products derived from
+ *      this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ *  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ *  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ *  OR BUSINESS INTERRUPTION). HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ *  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ *  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *****************************************************************************/
+/******************************************************************************
+ *
+ *
+ * @file mailbox_io.c
+ *
+ * Source file for mailbox IO for PYNQ Microblaze.
+ *
+ *
+ * <pre>
+ * MODIFICATION HISTORY:
+ *
+ * Ver   Who  Date     Changes
+ * ----- --- ------- -----------------------------------------------
+ * 1.00  po  02/08/18 release
+ *
+ * </pre>
+ *
+ *****************************************************************************/
 #include <stdint.h>
 #include <errno.h>
 #include <unistd.h>
@@ -72,13 +114,13 @@ static void volatile_cpy(volatile char* dest, volatile char* src, int len) {
 }
 
 int mailbox_available(int file) {
-	if (file < 0 || file >= MAX_DESCRIPTOR || descriptors[file].base_addr == NULL) {
+	if (file < 0 || file >= MAX_DESCRIPTOR || \
+    descriptors[file].base_addr == NULL) {
 		errno = EBADF;
 		return -1;
 	}
 	volatile int32_t* ctrl = (volatile int32_t*)descriptors[file].base_addr;
 	volatile int32_t* status = ctrl + 1;
-	volatile char* buffer = (volatile char*)(status + 1);
 	int buf_size = descriptors[file].size;
 	int available = 0;
 	int read_stream = descriptors[file].flags == O_RDONLY;
@@ -86,7 +128,6 @@ int mailbox_available(int file) {
 	// The BRAM can produce rubbish when a read/write collision happens
 	// so read twice to make sure that the available data if valid.
 	int last_available = 0xFFFF;
-	int count = 0;
 	while (last_available != available) {
 		last_available = available;
 		if (read_stream) {
@@ -100,7 +141,9 @@ int mailbox_available(int file) {
 }
 
 ssize_t mailbox_write(int file, const void* ptr, size_t len) {
-	if (file < 0 || file >= MAX_DESCRIPTOR || descriptors[file].flags != O_WRONLY || descriptors[file].base_addr == NULL) {
+	if (file < 0 || file >= MAX_DESCRIPTOR || \
+    descriptors[file].flags != O_WRONLY || \
+    descriptors[file].base_addr == NULL) {
 		errno = EBADF;
 		return -1;
 	}
@@ -110,14 +153,14 @@ ssize_t mailbox_write(int file, const void* ptr, size_t len) {
 	int buf_size = descriptors[file].size;
 
 	int available = mailbox_available(file);
-	int count = 0;
 	while (available == 0) {
 		available = mailbox_available(file);
 		_handle_events();
 	}
 	int write_ptr = *ctrl;
-	int to_write = len < available? len : available;
-	int first_block = to_write < (buf_size - write_ptr)? to_write: buf_size - write_ptr;
+	int to_write = (int)len < available? (int)len : available;
+	int first_block = \
+    to_write < (buf_size - write_ptr)? to_write: buf_size - write_ptr;
 	volatile_cpy(buffer + write_ptr, (char*)ptr, first_block);
 	if (first_block < to_write) {
 		volatile_cpy(buffer, (char*)ptr + first_block, to_write - first_block);
@@ -134,7 +177,9 @@ ssize_t mailbox_write(int file, const void* ptr, size_t len) {
 }
 
 ssize_t mailbox_read(int file, void* ptr, size_t len) {
-	if (file < 0 || file >= MAX_DESCRIPTOR || descriptors[file].flags != O_RDONLY || descriptors[file].base_addr == NULL) {
+	if (file < 0 || file >= MAX_DESCRIPTOR || \
+    descriptors[file].flags != O_RDONLY || \
+    descriptors[file].base_addr == NULL) {
 		errno = EBADF;
 		return -1;
 	}
@@ -150,8 +195,9 @@ ssize_t mailbox_read(int file, void* ptr, size_t len) {
 		_handle_events();
 	}
 	int read_ptr = *status;
-	int to_read = len < available? len : available;
-	int first_block = to_read < (buf_size - read_ptr)? to_read: buf_size - read_ptr;
+	int to_read = (int)len < available? (int)len : available;
+	int first_block = \
+    to_read < (buf_size - read_ptr)? to_read: buf_size - read_ptr;
 	volatile_cpy((char*)ptr, buffer + read_ptr, first_block);
 	if (first_block < to_read) {
 		volatile_cpy((char*)ptr + first_block, buffer, to_read - first_block);
