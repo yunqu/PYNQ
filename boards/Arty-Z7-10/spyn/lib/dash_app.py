99902d35a55801f74b97711fc3ea6d58622116f1
Kiran Vishal Thanjavur Bhaaskar <vishal94@bu.edu>
2018-05-10 16:49:29 -0700

    Update dash_app.py

--- boards/Arty-Z7-10/spyn/lib/dash_app.py
+++ boards/Arty-Z7-10/spyn/lib/dash_app.py
@@ -40,7 +40,7 @@ import os
 from pynq import Overlay
 from pynq import MMIO
 import numpy as np
-from spyn import *
+from pynq.lib.spyn import Motor_Controller
 from pynq import Xlnk
 from flask import send_from_directory
 
